9669c9309cea9e98e0112fabe8d97bf28d35e8bd
Volio12 <j.jakub66@gmail.com>
2022-12-03 14:58:59 +0100

    added correct form for adding prize

--- src/main/java/com/example/projekt/controllers/NobelPrizeController.java
+++ src/main/java/com/example/projekt/controllers/NobelPrizeController.java
@@ -1,6 +1,8 @@
 package com.example.projekt.controllers;
 
+import com.example.projekt.dao.ILaureateRepository;
 import com.example.projekt.dao.INobelPrizeRepository;
+import com.example.projekt.entities.Laureate;
 import com.example.projekt.entities.NobelPrize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,6 +20,9 @@ public class NobelPrizeController {
     @Autowired
     INobelPrizeRepository nobelPrizeRepository;
 
+    @Autowired
+    ILaureateRepository laureateRepository;
+
     @GetMapping
     public String displayPrizes(Model model){
         List<NobelPrize> prizes = nobelPrizeRepository.findAll();
@@ -28,7 +33,9 @@ public class NobelPrizeController {
     @GetMapping("/newPrize")
     public String displayPrizeForm(Model model){
         NobelPrize prize = new NobelPrize();
+        List<Laureate> laureates = laureateRepository.findAll();
         model.addAttribute("prize",prize);
+        model.addAttribute("laureates",laureates);
         return "/prizes/new-prize";
     }
 
